8f5779acb539a9370c2ec61a76b76772feeed9e4
akanksha28013 <akankshahake28@gmail.com>
2025-08-10 14:31:41 +0530

    Delete Day18-08-08-25 directory

--- Day18-08-08-25/Excercise2/letsgo/src/main/java/com/wipro/letsgo/repo/VehicleMovementRepo.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.letsgo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.letsgo.entity.VehicleMovement;
-
-public interface VehicleMovementRepo extends JpaRepository<VehicleMovement, Long> {
-
-}
